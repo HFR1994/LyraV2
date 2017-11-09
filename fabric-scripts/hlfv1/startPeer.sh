40f1ffea7a92421373c7081a8519f46c9365aba2
Héctor Flores <frhectorin@gmail.com>
2017-11-09 01:48:24 -0600

    Change Peer

---
+++ fabric-scripts/hlfv1/startPeer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -ev
+
+#Detect architecture
+ARCH=`uname -m`
+
+# Grab the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+cd "${DIR}"/composer
+
+ARCH=$ARCH docker-compose -f "${DIR}"/peers/docker-compose.yml down
+ARCH=$ARCH docker-compose -f "${DIR}"/peers/docker-compose.yml up -d
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=15
+echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
+cd ../..
